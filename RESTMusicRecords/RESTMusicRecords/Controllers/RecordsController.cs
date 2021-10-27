6b13c0e74d9a229f8cc79deab508906f27051bad
jakobln <62882745+jakobln@users.noreply.github.com>
2021-10-27 12:41:12 +0200

    Controller + Manager

---
+++ RESTMusicRecords/RESTMusicRecords/Controllers/RecordsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RESTMusicRecords.Managers;
+using RESTMusicRecords.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace RESTMusicRecords.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly RecordsManager _manager = new RecordsManager();
+
+
+        // GET: api/<ItemsController>
+        [HttpGet]
+        public IEnumerable<Records> Get([FromQuery] string substring)
+        {
+            return _manager.GetAll(substring);
+        }
+
+        // GET api/<ItemsController>/5
+        [HttpGet("{id}")]
+        public Records Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<ItemsController>
+        [HttpPost]
+        public Records Post([FromBody] Records value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<ItemsController>/5
+        [HttpPut("{id}")]
+        public Records Put(int id, [FromBody] Records value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<ItemsController>/5
+        [HttpDelete("{id}")]
+        public Records Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ RESTMusicRecords/RESTMusicRecords/Managers/RecordsManager.cs
@@ -0,0 +1,24 @@
+﻿using RESTMusicRecords.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTMusicRecords.Managers
+{
+    public class RecordsManager
+    {
+        private static readonly List<Records> Data = new List<Records>
+        {
+            new Records {Title = "string1", Artist = "artist1", Duration = 12, PublicationYear = 1999},
+            new Records {Title = "title2", Artist = "artist2", Duration = 22, PublicationYear = 1923}
+        };
+
+        public List<Records> GetAll(string substring)
+        {
+            return new List<Records>(Data);
+        }
+
+
+    }
+}
