ad51736b3428d5c14280b9a24184e6579e377ea6
jakobln <62882745+jakobln@users.noreply.github.com>
2021-10-27 12:27:45 +0200

    Models.cs class

---
+++ RESTMusicRecords/RESTMusicRecords/Models/Records.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTMusicRecords.Models
+{
+    public class Records
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+    }
+}
