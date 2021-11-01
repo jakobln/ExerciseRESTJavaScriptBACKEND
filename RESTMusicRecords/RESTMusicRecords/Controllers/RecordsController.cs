c6020215afc3a832d77128dbf2ce09d78b5101be
Nicolas Lauridsen <nicolas.lauridsen@gmail.com>
2021-11-01 11:14:11 +0100

    stuff

--- RESTMusicRecords/RESTMusicRecords/Controllers/RecordsController.cs
+++ RESTMusicRecords/RESTMusicRecords/Controllers/RecordsController.cs
@@ -6,13 +6,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace RESTMusicRecords.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ItemsController : ControllerBase
+    public class RecordsController : ControllerBase
     {
         private readonly RecordsManager _manager = new RecordsManager();
 
@@ -24,32 +24,5 @@ namespace RESTMusicRecords.Controllers
             return _manager.GetAll(substring);
         }
 
-        // GET api/<ItemsController>/5
-        [HttpGet("{id}")]
-        public Records Get(int id)
-        {
-            return _manager.GetById(id);
-        }
-
-        // POST api/<ItemsController>
-        [HttpPost]
-        public Records Post([FromBody] Records value)
-        {
-            return _manager.Add(value);
-        }
-
-        // PUT api/<ItemsController>/5
-        [HttpPut("{id}")]
-        public Records Put(int id, [FromBody] Records value)
-        {
-            return _manager.Update(id, value);
-        }
-
-        // DELETE api/<ItemsController>/5
-        [HttpDelete("{id}")]
-        public Records Delete(int id)
-        {
-            return _manager.Delete(id);
-        }
     }
 }
